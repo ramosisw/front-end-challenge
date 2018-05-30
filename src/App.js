67191cd8a6ed85ade6a01768bd4bd1aa02810bda
Julio C. Ramos <ramos.isw@gmail.com>
2018-05-29 23:59:10 -0500

    Integrando bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Component} from "react";
+import React, {Component} from "react";
 import "./App.css";
+import { Button } from 'reactstrap';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <Button color="danger">Hello World!</Button>
+        );
+    }
 }
 
 export default App;
