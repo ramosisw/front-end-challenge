f8333060c7cf718ac23197a1aad35e7195799caa
Julio C. Ramos <ramos.isw@gmail.com>
2018-06-04 00:26:59 -0500

    Show last trades

--- webpack.gitdoc.js
+++ webpack.gitdoc.js
@@ -5,7 +5,6 @@ const config = require('./webpack.common.js');
 const merge = require('webpack-merge');
 
 module.exports = merge(config, {
-    devtool: 'source-map',
     mode: 'production',
     output: {
         publicPath: path.resolve(__dirname, './docs/dist/')
